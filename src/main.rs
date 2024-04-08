38ee566ce172eacf5e65cdbe8a4117a4fc564cd4
Adam Hott <24814301+CodingInGreen@users.noreply.github.com>
2024-04-08 12:02:13 +0100

    Fix stand-alone example project

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,111 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+use anyhow::Result;
+use esp_idf_svc::{
+    hal::{
+        gpio::{InterruptType, PinDriver, Pull},
+        peripherals::Peripherals,
+        task::notification::Notification,
+    },
+    sys::esp_random,
+};
+use std::num::NonZeroU32;
+use core::time::Duration;
+use esp_idf_svc::hal::{
+    gpio::OutputPin,
+    peripheral::Peripheral,
+    rmt::{config::TransmitConfig, FixedLengthSignal, PinState, Pulse, RmtChannel, TxRmtDriver},
+};
+
+
+use rgb::RGB8;
+
+struct WS2812RMT<'a> {
+    tx_rtm_driver: TxRmtDriver<'a>,
+}
+
+impl<'d> WS2812RMT<'d> {
+    // Rust ESP Board gpio2,  ESP32-C3-DevKitC-02 gpio8
+    pub fn new(
+        led: impl Peripheral<P = impl OutputPin> + 'd,
+        channel: impl Peripheral<P = impl RmtChannel> + 'd,
+    ) -> Result<Self> {
+        let config = TransmitConfig::new().clock_divider(2);
+        let tx = TxRmtDriver::new(channel, led, &config)?;
+        Ok(Self { tx_rtm_driver: tx })
+    }
+
+    pub fn set_pixel(&mut self, rgb: RGB8) -> Result<()> {
+        let color: u32 = ((rgb.g as u32) << 16) | ((rgb.r as u32) << 8) | rgb.b as u32;
+        let ticks_hz = self.tx_rtm_driver.counter_clock()?;
+        let t0h = Pulse::new_with_duration(ticks_hz, PinState::High, &ns(350))?;
+        let t0l = Pulse::new_with_duration(ticks_hz, PinState::Low, &ns(800))?;
+        let t1h = Pulse::new_with_duration(ticks_hz, PinState::High, &ns(700))?;
+        let t1l = Pulse::new_with_duration(ticks_hz, PinState::Low, &ns(600))?;
+        let mut signal = FixedLengthSignal::<24>::new();
+        for i in (0..24).rev() {
+            let p = 2_u32.pow(i);
+            let bit = p & color != 0;
+            let (high_pulse, low_pulse) = if bit { (t1h, t1l) } else { (t0h, t0l) };
+            signal.set(23 - i as usize, &(high_pulse, low_pulse))?;
+        }
+        self.tx_rtm_driver.start_blocking(&signal)?;
+
+        Ok(())
+    }
+}
+
+fn ns(nanos: u64) -> Duration {
+    Duration::from_nanos(nanos)
+}
+
+fn main() -> Result<()> {
     esp_idf_svc::sys::link_patches();
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+    let peripherals = Peripherals::take()?;
+    // ANCHOR: led
+    let mut led = WS2812RMT::new(peripherals.pins.gpio2, peripherals.rmt.channel1)?;
+    // ANCHOR_END: led
 
-    log::info!("Hello, world!");
+    // Configures the button
+    let mut button = PinDriver::input(peripherals.pins.gpio9)?;
+    button.set_pull(Pull::Up)?;
+    button.set_interrupt_type(InterruptType::PosEdge)?;
+
+    // Configures the notification
+    let notification = Notification::new();
+    let notifier = notification.notifier();
+
+    // Subscribe and create the callback
+    // Safety: make sure the `Notification` object is not dropped while the subscription is active
+    unsafe {
+        button.subscribe(move || {
+            notifier.notify_and_yield(NonZeroU32::new(1).unwrap());
+        })?;
+    }
+
+    // ANCHOR: loop
+    loop {
+        // Enable interrupt and wait for new notificaton
+        button.enable_interrupt()?;
+        notification.wait(esp_idf_svc::hal::delay::BLOCK);
+        println!("Button pressed!");
+        // Generates random rgb values and sets them in the led.
+        random_light(&mut led);
+    }
+    // ANCHOR_END: loop
 }
+
+#[allow(unused)]
+// ANCHOR: random_light
+fn random_light(led: &mut WS2812RMT) {
+    let mut color = RGB8::new(0, 0, 0);
+    unsafe {
+        let r = esp_random() as u8;
+        let g = esp_random() as u8;
+        let b = esp_random() as u8;
+
+        color = RGB8::new(r, g, b);
+    }
+
+    led.set_pixel(color).unwrap();
+}
+// ANCHOR_END: random_light
